4e6dc92bef849cf0ab07b0d3523526ae5d948fef
Pyunghwa Kim <syneffort@gmail.com>
2021-05-16 22:52:20 +0900

    crypto and util

---
+++ chapt2/util.js
@@ -0,0 +1,17 @@
+const util = require('util');
+const crypto = require('crypto');
+
+const dontUseMe = util.deprecate((x, y) => {
+    console.log(x + y);
+}, 'dontUseMe 함수는 deprecated 됨!');
+dontUseMe(1, 2);
+
+
+const randomBytesPromise = util.promisify(crypto.randomBytes);
+randomBytesPromise(64)
+    .then((buf) => {
+        console.log(buf.toString('base64'));
+    })
+    .catch((error) => {
+        console.error(error);
+    })
