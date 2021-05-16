6f65fd674de7d60eadc71194efa3ff030a8c15e2
Pyunghwa Kim <syneffort@gmail.com>
2021-05-16 20:44:40 +0900

    module and circular dependency

---
+++ chapt2/require.js
@@ -0,0 +1,10 @@
+require('../chapt1/var');
+console.log(require);
+
+console.log(require.cache);
+
+console.log(require.main);
+
+console.log(require.main == module);
+
+console.log(require.main.filename);
