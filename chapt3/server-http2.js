57c865ec12b3d52380c20c917c472454edf4ea4a
Pyunghwa Kim <syneffort@gmail.com>
2021-05-27 11:28:51 +0900

    session, https, http2

---
+++ chapt3/server-http2.js
@@ -0,0 +1,17 @@
+const http2 = require('http2');
+const fs = require('fs');
+
+http2.createSecureServer({
+    cert: fs.readdirSync('인증서 경로'),
+    key: fs.readFileSync('비밀키 경로'),
+    ca: [
+        fs.readFileSync('상위 인증서 경로'),
+        fs.readFileSync('상위 인증서 경로'),
+    ],
+}, (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end('https 서버입니다.');
+})
+    .listen(443, () => {
+        console.log('443포트에서 대기 중...');
+    });
