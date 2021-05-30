c9f295eba8f566c41336a9e7dbe298a3e76b0cc7
Pyunghwa Kim <syneffort@gmail.com>
2021-05-30 17:17:09 +0900

    multer

--- chapt5/learn-express/app.js
+++ chapt5/learn-express/app.js
@@ -48,6 +48,35 @@ app.use(express.urlencoded({ extended: true })); // true면 qs, false면 queryst
 //     }
 // });
 
+const multer = require('multer');
+const fs = require('fs');
+
+try {
+    fs.readdirSync('uploads');
+} catch (err) {
+    console.error('uploads 폴더가 없어 새로 생성합니다.');
+    fs.mkdirSync('uploads');
+}
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'uploads/');
+        },
+        filename(req, file, done) {
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
+app.get('/upload', (req, res) => {
+    res.sendFile(path.join(__dirname, 'multipart.html'));
+});
+app.post('/upload', upload.array('image'), (req, res) => {
+    console.log(req.files);
+    res.send('ok');
+})
+
 app.use((req, res, next) => {
     req.data = 'session 비번';
     next();
