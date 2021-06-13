ff61c301850b5eb9d8dfac15fb54fda9b2c8c42b
Pyunghwa Kim <syneffort@gmail.com>
2021-06-13 18:25:32 +0900

    hashtag

--- chapt8/nodebird/routes/post.js
+++ chapt8/nodebird/routes/post.js
@@ -40,6 +40,18 @@ router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
             img: req.body.url,
             UserId: req.user.id,
         });
+        const hashtags = req.body.content.match(/#[^\s#]*/g);
+        if (hashtags) {
+            const result = await Promise.all(
+                hashtags.map(tag => {
+                    return Hashtag.findOrCreate({
+                        where: { title: tag.slice(1).toLowerCase() }, // # 제거 후 소문자
+                    });
+                }),
+            );
+            console.log(result);
+            await post.addHashtags(result.map(r => r[0]));
+        }
         res.redirect('/');
     } catch (error) {
         console.error(error);
