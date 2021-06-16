cc187c0cb29b714609467c188c371d7e9c2f55b3
Pyunghwa Kim <syneffort@gmail.com>
2021-06-16 22:18:24 +0900

    api limiter

--- chapt9/nodebird-api/app.js
+++ chapt9/nodebird-api/app.js
@@ -11,6 +11,7 @@ dotenv.config();
 const authRouter = require('./routes/auth');
 const indexRouter = require('./routes/index');
 const v1Router = require('./routes/v1');
+const v2Router = require('./routes/v2');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
@@ -50,6 +51,7 @@ app.use(passport.session());
 app.use('/auth', authRouter);
 app.use('/', indexRouter);
 app.use('/v1', v1Router);
+app.use('/v2', v2Router);
 
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
