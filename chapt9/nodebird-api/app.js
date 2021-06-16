cef6e901b2cfdfcce9badba8de98fc6835483a66
Pyunghwa Kim <syneffort@gmail.com>
2021-06-16 23:15:54 +0900

    CORS 해결

--- chapt9/nodebird-api/app.js
+++ chapt9/nodebird-api/app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 const authRouter = require('./routes/auth');
@@ -47,6 +48,10 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(cors({
+    origin: true,
+    credential: true,
+}));
 
 app.use('/auth', authRouter);
 app.use('/', indexRouter);
