26d41635b5e6631373cf371500593af29a67801b
washswat-peace <syneffort@gmail.com>
2021-06-19 12:49:15 +0900

    database mocking

--- chapt10/nodebird/controllers/user.test.js
+++ chapt10/nodebird/controllers/user.test.js
@@ -1,4 +1,6 @@
 const { addFollowing } = require('./users');
+jest.mock('../models/user');
+const User = require('../models/user');
 
 describe('addFollowing', () => {
     const req = {
@@ -11,16 +13,24 @@ describe('addFollowing', () => {
     };
     const next = jest.fn();
     test('사용자를 찾아 팔로잉을 추가하고 res.send(success)를 응답해야 함', async () => {
+        User.findOne.mockReturnValue(Promise.resolve({ 
+            id: 1, 
+            name: 'mockinguser', 
+            addFollowings(value) {
+                return Promise.resolve(true);
+        }}));
         await addFollowing(req, res, next);
         expect(res.send).toBeCalledWith('success');
     });
     test(`사용자를 찾지 못하면 res.status(404).send('no user')를 응답해야 함`, async () => {
+        User.findOne.mockReturnValue(Promise.resolve(null));
         await addFollowing(req, res, next);
         expect(res.status).toBeCalledWith(404);
         expect(res.send).toBeCalledWith('no user');
     });
     test('DB 에러 시 next(error) 호출해야 함', async () => {
         const error = 'test error';
+        User.findOne.mockReturnValue(Promise.reject(error));
         await addFollowing(req, res, next);
         expect(next).toBeCalledWith(error);
     });
