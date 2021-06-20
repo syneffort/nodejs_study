bff494cac97ec3b73be73ff0bf68680a89eec8b1
washswat-peace <syneffort@gmail.com>
2021-06-20 20:16:31 +0900

    socket.io

--- chapt11/gif-chat/socket.js
+++ chapt11/gif-chat/socket.js
@@ -1,26 +1,24 @@
-const WebSocket = require('ws');
+const SocketIO = require('socket.io');
 
 module.exports = (server) => {
-    const wss = new WebSocket.Server({ server });
+    const io = SocketIO(server, { path: '/socket.io' });
 
-    wss.on('connection', (ws, req) => {
+    io.on('connection', (socket) => {
+        const req = socket.request;
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log('새로운 클라이언트 접속', ip);
-        ws.on('message', (message) => {
-            console.log(message);
+        console.log('새로운 클라이언트 접속!', ip, socket.id, req.id);
+        socket.on('disconnect', () => {
+            console.log('클라이언트 접속 해제', ip, socket.id);
+            clearInterval(socket.interval);
         });
-        ws.on('error', (error) => {ß
+        socket.on('error', (error) => {
             console.error(error);
         });
-        ws.on('close', () => {
-            console.log('클라이언트 접속 해제', ip);
-            clearInterval(ws.interval);
+        socket.on('reply', (data) => {
+            console.log(data);
         });
-
-        ws.interval = setInterval(() => {
-            if (ws.readyState === ws.OPEN) {
-                ws.send(`[${new Date()}] 서버에서 클라이언트로 메세지를 보냅니다.`);
-            }
+        socket.interval = setInterval(() => {
+            socket.emit('news', 'Hello Socket.IO');
         }, 3000);
-    })
+    });
 };
